8dad4cbf06e55ec064bcdef49e5df685d098e8fc
Jeferson Costa <jefersonbrz.costa@gmail.com>
2020-09-23 06:01:12 -0300

    add to root reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,11 +1,12 @@
-import {all} from "redux-saga/effects";
-import {combineReducers} from "redux";
+import { all } from "redux-saga/effects";
+import { combineReducers } from "redux";
 
 import * as auth from "../app/modules/Auth/_redux/authRedux";
-
+import clientesReducer from "../app/modules/Administrativo/clientes/_redux/clientesReducer";
 
 export const rootReducer = combineReducers({
   auth: auth.reducer,
+  clientes: clientesReducer,
 });
 
 export function* rootSaga() {
